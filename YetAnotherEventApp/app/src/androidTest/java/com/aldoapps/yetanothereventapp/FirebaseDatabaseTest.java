2a22112e5fcea7b806244ce11b59dbab2b9bb8f9
Aldo Wachyudi <aldo.wachyudi@icehousecorp.com>
2016-10-27 18:48:47 +0700

    clean up

--- YetAnotherEventApp/app/src/androidTest/java/com/aldoapps/yetanothereventapp/FirebaseDatabaseTest.java
+++ YetAnotherEventApp/app/src/androidTest/java/com/aldoapps/yetanothereventapp/FirebaseDatabaseTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
